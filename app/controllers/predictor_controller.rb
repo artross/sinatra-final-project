3d212e89a2a6667709a7d8bd9862f232bfbf4250
Artem Eremin <artross.bodhi@gmail.com>
2016-02-24 16:22:27 +0300

    predictions

--- app/controllers/predictor_controller.rb
+++ app/controllers/predictor_controller.rb
@@ -8,13 +8,14 @@ class PredictorController < ApplicationController
   end
 
   get '/index' do
-  	@current_user ||= User.find_by(id: session[:id])
+  	@current_user = User.find_by(id: session[:id])
+  	@current_users_place = TableHelper.find_place_by_id(session[:id])
   	erb :'general/index.html'
   end
 
   get '/results/:user_id' do
-  	@current_user ||= User.find_by(id: session[:id])
-  	@selected_user ||= @current_user
+  	@current_user = User.find_by(id: session[:id])
+  	@selected_user = User.find_by(id: params[:user_id])
   	erb :'general/results.html'
   end
 
@@ -24,15 +25,44 @@ class PredictorController < ApplicationController
   end
 
   get '/predictions' do
-  	404 #erb :'predictions'
+  	@current_user = User.find_by(id: session[:id])
+  	@fixtures_to_predict = TableHelper.fixtures_to_predict(session[:id])
+  	erb :'general/predictions.html'
   end
   
   post '/predictions' do
-    404
+    params[:predictions].each do |p|
+      unless p[:home] == "" || p[:away] == "" then
+      	prediction = Prediction.find_by(fixture_id: p[:id], user_id: session[:id])
+      	if prediction then
+      	  unless prediction.home_team_goals == p[:home].to_i && prediction.away_team_goals == p[:away].to_i then
+      	    prediction.home_team_goals = p[:home].to_i
+      	    prediction.away_team_goals = p[:away].to_i
+      	    prediction.save
+      	  end  
+      	else
+      	  Prediction.create(
+      	  	{
+      	  	  user_id: session[:id], 
+      	  	  fixture_id: p[:id], 
+      	  	  home_team_goals: p[:home], 
+      	  	  away_team_goals: p[:away], 
+      	  	  prediction_points: 0
+      	  	})
+      	end  	  
+      end
+    end
+    redirect to('/predictions')
   end	
 
   def logged_in?
   	session[:id] && User.find_by(id: session[:id])
+  end
+
+  def supported_leagues
+  	current_year = Time.now.year
+    arr = League.where("year = ?", current_year.to_s)
+    arr = League.where("year = ?", (current_year - 1).to_s) if arr.size == 0
   end
 
 end
--- app/lib/table_helper.rb
+++ app/lib/table_helper.rb
@@ -2,4 +2,62 @@ class TableHelper
   def self.find_place_by_id(user_id)
   	1
   end
+  
+  def self.fixtures_to_predict(user_id)
+    conn = ActiveRecord::Base.connection
+    query = <<-SQL
+      SELECT
+        f.id
+        , f.date
+        , home_t.short_name home_team
+        , away_t.short_name away_team
+        , p.home_team_goals
+        , p.away_team_goals
+      FROM
+        fixtures f 
+        INNER JOIN teams home_t ON f.home_team_id = home_t.id
+        INNER JOIN teams away_t ON f.away_team_id = away_t.id
+        LEFT OUTER JOIN predictions p ON f.id = p.fixture_id AND p.user_id = #{conn.quote(user_id)}
+      WHERE
+        f.home_team_goals IS NULL
+      ORDER BY
+        f.date, f.id
+    SQL
+    
+    conn.execute(query)
+  end
+
+  def self.results_with_filters(params)
+    conn = ActiveRecord::Base.connection
+
+    params[:days] = 7 if params[:days] <= 0
+    date_condition_str = (Time.now - 60*60*24*params[:days]).strftime("%Y-%m-%d 00:00:00")
+    
+    query = <<-SQL
+      SELECT
+        f.date
+        , home_t.short_name
+        , away_t.short_name
+        , f.home_team_goals
+        , f.away_team_goals
+        , p.home_team_goals
+        , p.away_team_goals
+        , p.prediction_points
+      FROM
+        predictions p
+        INNER JOIN fixtures f ON p.fixture_id = f.id
+        INNER JOIN teams home_t ON f.home_team_id = home_t.id
+        INNER JOIN teams away_t ON f.away_team_id = away_t.id
+      WHERE
+        p.user_id = #{conn.quote(User.find_by(name: params[:username]).id)}
+        AND f.date >= #{conn.quote(date_condition_str)}
+        AND f.home_team_goals IS NOT NULL
+    SQL
+
+    if params[:league_id] > 0 then
+      query += "AND f.league_id = #{conn.quote(params[:league_id])}"
+    end  
+
+    qres = ActiveRecord::Base.connection.execute(query)
+  end
 end
